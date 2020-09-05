597c5e78029229d86be60502fea2df257f38183e
meghrajfalle <meghraj.falle@appdirect.com>
2020-09-06 00:19:57 +0530

    Branch1: Adding basing routes for users.js, auth.js and contacts.js resources

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = express();
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Contact Keeper API!' })
 );
-
+//Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
