713940aa5bee20e6efc25bea22b16b58f74edc21
meghrajfalle <meghraj.falle@appdirect.com>
2020-09-07 02:49:22 +0530

    Branch9: Added POST api/contacts logic and returned the added user as json response. Did checks on the request body field name

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,7 +7,7 @@ const User = require('../models/User');
 const Contact = require('../models/Contact');
 
 // @route       GET api/contacts
-// @desc        Get all users contacts
+// @desc        Get all user's contacts
 // @access      Private
 router.get('/', auth, async (req, res) => {
   try {
@@ -24,9 +24,42 @@ router.get('/', auth, async (req, res) => {
 // @route       POST api/contacts
 // @desc        Add new contact
 // @access      Private
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route       PUT api/contacts/:id
 // @desc        Update a contact
