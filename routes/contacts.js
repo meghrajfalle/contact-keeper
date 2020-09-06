db6c336d61e881c2292ccfaa4187aabb29ec31df
meghrajfalle <meghraj.falle@appdirect.com>
2020-09-07 02:13:10 +0530

    Branch8: Create Contact Model and make the Get all contact of user - /api/contacts protected and provide the result in response

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route       GET api/contacts
 // @desc        Get all users contacts
 // @access      Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route       POST api/contacts
