e7deb9a94af941b253ab51ddee78fe4d8cc61a28
meghrajfalle <meghraj.falle@appdirect.com>
2020-09-15 00:40:25 +0530

    Branch17: Created current variable in the ContactState.js and added the setCurrent and clearCurrent function which dispatches types - SET_CURRENT, CLEAR_CURRENT to reducer and created switch case for them

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -6,10 +6,11 @@ const ContactItem = ({ contact }) => {
   const contactContext = useContext(ContactContext);
 
   const { id, name, email, phone, type } = contact;
-  const { deleteContact } = contactContext;
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
   const onDelete = e => {
     deleteContact(id);
+    clearCurrent();
   };
 
   return (
@@ -40,7 +41,13 @@ const ContactItem = ({ contact }) => {
         )}
       </ul>
       <p>
-        <button className='btn btn-dark btn-sm'> Edit</button>
+        <button
+          className='btn btn-dark btn-sm'
+          onClick={() => setCurrent(contact)}
+        >
+          {' '}
+          Edit
+        </button>
         <button className='btn btn-danger btn-sm' onClick={onDelete}>
           {' '}
           Delete
