89eac863cdf07f12b2ca804565b9adf53e9b841b
meghrajfalle <meghraj.falle@appdirect.com>
2020-09-14 10:16:31 +0530

    Branch15: Added ContactForm.js and defined addContact function in ContactState and exposed it . Also, added switch case in contactReducer for the ADD_CONTACT type.

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -42,6 +42,10 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   //Add Contact
+  const addContact = contact => {
+    contact.id = v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   //Delete Contact
 
@@ -56,7 +60,7 @@ const ContactState = props => {
   //Clear Filter
 
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts }}>
+    <ContactContext.Provider value={{ contacts: state.contacts, addContact }}>
       {props.children}
     </ContactContext.Provider>
   );
